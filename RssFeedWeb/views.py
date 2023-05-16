d1e19691d3de6889d918bf024bc14b313f8be58c
WAME-LEL <fnskh0606@gmail.com>
2023-05-17 00:51:58 +0900

    add css

--- RssFeedWeb/views.py
+++ RssFeedWeb/views.py
@@ -13,26 +13,51 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 import feedparser
+from bs4 import BeautifulSoup
+
 
 def rss_feed(request):
     # RSS 피드 주소
-    rss_url = "https://computer-science-student.tistory.com/rss"
-
+    # rss_url = "https://computer-science-student.tistory.com/rss"
+    rss_url = "https://joel-dev.site/rss"
     # RSS 피드 파싱
     feed = feedparser.parse(rss_url)
 
-    # HTML 파싱
-    # res = requests.get(feed.entries[0].description)
-    # soup = BeautifulSoup(res.text, "html.parser")
 
-    # 원하는 정보 가져오기
-    # title = soup.select_one('pre id="code_1651423605149"').text
 
     # 최신 글 하나 가져오기
     latest_entry = feed.entries[0]
+    second = feed.entries[1]
+    third = feed.entries[2]
+
+    entries = []
+
+    for entry in feed.entries:
+        # 항목의 내용(content)에서 <p> 태그의 데이터만 추출
+        content = entry.description
+        soup = BeautifulSoup(content, 'html.parser')
+        p_tags = soup.find_all('p')
+
+        paragraphs = []
+
+        # <p> 태그의 데이터를 paragraphs 리스트에 추가
+        for p in p_tags:
+            if p.string != '\xa0' and p.string is not None:  # &nbsp; 태그 거르기
+                paragraphs.append(p.string)
+
+        entries.append({
+            'title': entry.title,
+            'paragraphs': paragraphs
+        })
+
+    context = {
+        'entries': entries
+    }
+
+
 
     # 템플릿 렌더링
-    return render(request, "RssFeedWeb/rss_feed.html", {"feed": feed, "latest_entry": latest_entry})
+    return render(request, "RssFeedWeb/rss_feed.html", {"feed": feed, "latest_entry": latest_entry, "second": second, "third": third, "paragraphs": paragraphs, "context": context})
 
 #구독 버튼, 돌아오는 버튼 이벤트
 def sub(request):
